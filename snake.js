72aed0ad253951d3424b952399df25274fc47edf
Vijay Pal <vijay.nsut@gmail.com>
2021-06-29 14:38:08 +0530

    initial Setup

---
+++ snake.js
@@ -0,0 +1,24 @@
+// defining the initial objects and state
+function init(){
+    var canvas = document.getElementById('mycanvas');
+    W = canvas.height = 1000;
+    H = canvas.width = 1000;
+    pen = canvas.getContext('2d');
+
+
+}
+
+function draw(){
+
+}
+
+function update(){
+
+}
+
+function gameloop(){
+
+}
+
+init();
+var f= setInterval(gameloop,100);
